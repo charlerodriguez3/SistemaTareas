f0bbf15baf7f2a3720fe5b3252fda576448df202
charlerodriguez3 <andres199924@hotmail.com>
2024-09-26 22:09:09 -0500

    Validación Token en Swagger para Consumir servicios

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -30,33 +30,34 @@ namespace SistemaTareas.Controllers
 
                 var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.NombreUsuario == login.Username);
 
-                if (login.Username == usuario.NombreUsuario  && login.Password == usuario.PassUsuario) 
+                if (usuario is not null)
                 {
-
-                    var claims = new[]
+                    if (login.Username == usuario.NombreUsuario && login.Password == usuario.PassUsuario)
                     {
-                    new Claim(JwtRegisteredClaimNames.Sub, login.Username),
-                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-                };
+
+                        var claims = new[]
+                        {
+                            new Claim(JwtRegisteredClaimNames.Sub, login.Username),
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                        };
 
 
-                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("k6VwneyHMqDfFAvW9nPOz616zAAvifH5")); 
-                    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("k6VwneyHMqDfFAvW9nPOz616zAAvifH5"));
+                        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
+                        var token = new JwtSecurityToken(
+                            issuer: "SistemaTareas",
+                            audience: "Usuario",
+                            claims: claims,
+                            expires: DateTime.Now.AddMinutes(30),
+                            signingCredentials: creds);
 
-                    var token = new JwtSecurityToken(
-                        issuer: "SistemaTareas",  
-                        audience: "Usuario",
-                        claims: claims,
-                        expires: DateTime.Now.AddMinutes(30),
-                        signingCredentials: creds);
-
-                    return Ok(new
-                    {
-                        token = new JwtSecurityTokenHandler().WriteToken(token)
-                    });
+                        return Ok(new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(token)
+                        });
+                    }
                 }
-
                 return Unauthorized();
             }
         }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 
+using DB.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 namespace SistemaTareas
@@ -28,11 +31,39 @@ namespace SistemaTareas
                 };
             });
 
+            builder.Services.AddDbContext<SistemaTareasContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("SistemaTareasDB")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mi API", Version = "v1" });
+
+                // Configurar la autenticación JWT
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Por favor, ingresa el token JWT en el formato: Bearer {token}",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        }, new string[] {}
+                    }
+                });
+            });
 
             builder.Services.AddCors(options =>
             {
